c96ff702e64107be937ba52199a4a46741ba038e
jiye-7 <jy522hi@gmail.com>
2023-06-15 16:31:16 +0900

    feat: css reset, router 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,16 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <h1>누구보다 빛나조</h1>;
+  return (
+    <BrowserRouter>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<div>Main ExamPage</div>} />
+        <Route path="/post" element={<div>Post ExamPage</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
