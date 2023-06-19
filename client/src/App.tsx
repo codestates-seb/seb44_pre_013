a0a5052b7865a9972d0f05761f249a899186eef6
novice-hero <highero700@gmail.com>
2023-06-19 15:25:11 +0900

    feat: 회원가입 페이지 css 완료

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
 import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
+import SignUpPage from './pages/SignUpPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/post" element={<div>Post ExamPage</div>} />
         <Route path="/auth/login" element={<LoginPage />} />
+        <Route path="/members/signup" element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
   );
