00b1e2916a286ca915b51997a1b7d75da86e5ec8
jiye-7 <jy522hi@gmail.com>
2023-06-21 09:50:43 +0900

    #9 router 수정 및 추가
    - 폴더 구조 변경으로 import 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import GlobalStyles from './styles/GlobalStyles';
-import Question from './pages/Question/';
+import Question from './pages/Question/WritePage';
+import QuestionDetail from './pages/Question/DetailPage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 
@@ -10,11 +11,10 @@ function App() {
     <BrowserRouter>
       <GlobalStyles />
       <Routes>
-        <Route path="/" element={<div>Main ExamPage</div>} />
-        <Route path="/question" element={<Question />} />
-        <Route path="/post" element={<div>Post ExamPage</div>} />
         <Route path="/auth/login" element={<LoginPage />} />
         <Route path="/members/signup" element={<SignUpPage />} />
+        <Route path="/questions" element={<Question />} />
+        <Route path="/questions/:id" element={<QuestionDetail />} />
       </Routes>
     </BrowserRouter>
   );
