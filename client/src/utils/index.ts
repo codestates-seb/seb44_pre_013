36cee38a0c374f7c6feb7749e0826838abfe6661
novice-hero <highero700@gmail.com>
2023-06-23 23:59:18 +0900

    feat: 유틸 함수 추가

---
+++ client/src/utils/index.ts
@@ -0,0 +1,19 @@
+const getHeader = (response: Response, str: string) => {
+  return response ? response.headers.get(str) : '';
+};
+
+const isStatusOK = (response: Response) => {
+  const isOK = response.status.toString()[0];
+  return isOK === '2' ? true : false;
+};
+
+const setLocalStorage = (key: string, token: any) => {
+  localStorage.setItem(key, JSON.stringify(token));
+};
+
+const getLocalStorage = <T>(key: string, initialValue: T) => {
+  const item = localStorage.getItem(key);
+  return item ? JSON.parse(item) : initialValue;
+};
+
+export { getHeader, isStatusOK, setLocalStorage, getLocalStorage };
