22c5601de265ab447fc33a7e0a03f7fd8b68d840
novice-hero <highero700@gmail.com>
2023-06-23 23:56:36 +0900

    feat: 로그인 상태에 따른 헤더 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
+import { useSelector } from 'react-redux';
+import { RootState } from './store/store';
 import RouteProvider from './Routes';
 import GlobalStyles from './styles/GlobalStyles';
 import BeforeLoginHeader from './components/BeforeLoginHeader';
+import AfterLoginHeader from './components/AfterLoginHeader';
 
 function App() {
+  const isLogin = useSelector((state: RootState) => state.login.isLogin);
   return (
     <>
       <GlobalStyles />
-      <RouteProvider>
-        <BeforeLoginHeader />
-      </RouteProvider>
+      <RouteProvider>{isLogin ? <AfterLoginHeader /> : <BeforeLoginHeader />}</RouteProvider>
     </>
   );
 }
