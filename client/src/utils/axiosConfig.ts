d1babd64a708bd74784867319041c321d236099b
jiye-7 <jy522hi@gmail.com>
2023-06-27 09:11:47 +0900

    #64 임시 header config
    - token 변수로 설정

--- client/src/utils/axiosConfig.ts
+++ client/src/utils/axiosConfig.ts
@@ -1,6 +1,6 @@
 export const config = {
   headers: {
     Authorization:
-      'Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sImlkIjoicXdlQGdtYWlsLmNvbSIsIm1lbWJlcklkIjoxLCJzdWIiOiJxd2VAZ21haWwuY29tIiwiaWF0IjoxNjg3NzYzMzYwLCJleHAiOjE2ODc3NjUxNjB9._82FwG3Z-d3uFt3RtFaEP0A-qKgrKJ78bWRKSqG5sOvEDX55bWAzLAD3vAAxs0p3jVIR8n0kldQ_Az4n1tym7Q',
+      'Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sImlkIjoidGVzdEBnbWFpbC5jb20iLCJtZW1iZXJJZCI6MSwic3ViIjoidGVzdEBnbWFpbC5jb20iLCJpYXQiOjE2ODc4MDMyODAsImV4cCI6MTY4NzgwNTA4MH0.9lGNESWf8CGo9i0GFKucmUBW5if5Ldd297wu01S21Eb8EOdyHNgbD0VNVW88JyKtucoHAOY_yIlh8_mbJPii-g',
   },
 };
